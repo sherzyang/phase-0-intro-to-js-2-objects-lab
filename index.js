d5681b5eef23a6a51cbe6cd0cfd2d608abdc47fc
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-06 11:15:11 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {};
+
+function  updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployees = {...employee}; 
+    newEmployees[key] = value;
+    return newEmployees;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+
+function  deleteFromEmployeeByKey(employee, key) {
+    const newEmployees = {...employee};
+    delete newEmployees[key];
+    return newEmployees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
